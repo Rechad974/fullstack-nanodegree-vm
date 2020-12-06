0b8a1ce7445c26c361c9bb00e1a48a58e829eb3a
Rechad <rechad.locate@outlook.com>
2020-12-06 16:54:14 +0400

    Updated readme

--- my_readme.md
+++ my_readme.md
@@ -10,3 +10,73 @@ Stop Vagrant
 
 SSH into VM
 `vagrant ssh`
+
+-- CRUD operation SQLAlchemy
+
+Import libs, create engine, create session
+
+```python
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantMenu.db')
+Base.metadata.bind=engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+```
+
+Create (add function)
+
+```python
+myFirstRestaurant = Restaurant(name = "Pizza Palace")
+session.add(myFirstRestaurant)
+sesssion.commit()
+```
+
+Read 
+
+```python
+### One result
+
+firstResult = session.query(Restaurant).first()
+firstResult.name
+
+### Multiple results
+items = session.query(MenuItem).all()
+for item in items:
+    print item.name
+```
+
+Update
+
+1) Find entry 
+2) Reset value
+3) Add to session (like for create value)
+4) Execute the session commit
+
+```python
+### Find result
+
+veggieBurgers = session.query(MenuItem).filter_by(name= 'Veggie Burger')
+for veggieBurger in veggieBurgers:
+    print veggieBurger.id
+    print veggieBurger.price
+    print veggieBurger.restaurant.name
+    print "\n"
+```
+  
+```python
+### Update entry
+
+UrbanVeggieBurger = session.query(MenuItem).filter_by(id=8).one()
+UrbanVeggieBurger.price = '$2.99'
+session.add(UrbanVeggieBurger)
+session.commit() 
+```
+
+Delete
+
+1. Find the entry
+2. Delete the entry
+3. Execute the session commit
--- vagrant/webserver.py
+++ vagrant/webserver.py
@@ -1,6 +1,17 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+## CRUD operations
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+## DB setup
+engine = create_engine('sqlite:///restaurantMenu.db')
+Base.metadata.bind=engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 class webserverHandler(BaseHTTPRequestHandler):
   def do_GET(self):
     try:
@@ -8,7 +19,6 @@ class webserverHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('content-type', 'text/html')
         self.end_headers()
-
         output = ""
         output += "<html><body>Hello!"
         output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form>"
@@ -17,20 +27,40 @@ class webserverHandler(BaseHTTPRequestHandler):
         self.wfile.write(output)
         print(output)
         return
-      if self.path.endswith("/hola"):
+      if self.path.endswith("/restaurants"):
         self.send_response(200)
         self.send_header('content-type', 'text/html')
         self.end_headers()
 
         output = ""
-        output += "<html><body>Hola! <a href='/hello'>Go back</a>"
-        output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form>"
+        output += "<html><body>"
+        output += "<h2>List of restaurants</h2>"
+        output += "<a href='/restaurants/new'>Make a new restaurant</a>"
+        restaurants = session.query(Restaurant).all()
+        for restaurant in restaurants:
+          output += "<h4>{}</h4>".format(restaurant.name)
+          output += "<ul>"
+          output += "<li><a href=''>Edit</a></li>"
+          output += "<li><a href=''>Delete</a></li>"
+          output += "</ul>"
         output += "</body></html>"
 
         self.wfile.write(output)
         print(output)
         return
+      if self.path.endswith("/restaurants/new"):
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
 
+        output = ""
+        output += "<html><body>"
+        output += "<form method='POST' enctype='multipart/form-data' action='/new'><h2>Make a new Restaurant</h2><input name='name' type='text'><input type='submit' value='Create'></form>"
+        output += "</body></html>"
+
+        self.wfile.write(output)
+        print(output)
+        return
     except IOError:
       self.send_error(404, "File not found {}".format(self.path))
 
